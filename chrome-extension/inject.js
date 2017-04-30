c99c4e690ff8bb2163c27bf856d507128fb843dc
Jesse Bangs <jaspax@gmail.com>
2017-04-30 17:30:45 +0300

    added in a working keyword chart with good hover data

--- chrome-extension/inject.js
+++ chrome-extension/inject.js
@@ -4,6 +4,7 @@ const prefix = 'ams-unlocked';
 const showHistoryClass = `${prefix}-showhistory`;
 const chartId = `${prefix}-chart`;
 const chartClass = `${prefix}-chart-btn`;
+const tabClass = `${prefix}-tab`;
 const span = {
     second: 1000,
     minute: 1000 * 60,
@@ -17,6 +18,9 @@ const charts = [
     { column: 10, label: "Sales / day", config: {metric: 'salesCount', rate: 'day'} },
 ];
 
+const getEntityId = () => getQueryArgs()['entityId'];
+const getCampaignId = () => getQueryArgs()['campaignId'];
+
 const unlockSvg = chrome.runtime.getURL('images/unlock.svg');
 
 window.setInterval(() => {
@@ -28,11 +32,17 @@ window.setInterval(() => {
         dashboard.append($(`<div id="${chartId}"></div>`));
     }
 
-    let keywordBtn = $('#showKeywordInputsButton');
-    if (keywordBtn.length && keywordBtn.siblings(`.${chartClass}`).length == 0) {
-        addKeywordButton(keywordBtn);
+    let campaignTabs = $('#campaign_detail_tab_set');
+    if (campaignTabs.length && campaignTabs.find(`.${tabClass}`).length == 0) {
+        addCampaignTabs(campaignTabs);
     }
 }, 100);
+
+chrome.runtime.sendMessage({
+    action: 'setSession', 
+    entityId: getEntityId(), 
+    cookies: document.cookie,
+});
 
 function getQueryArgs() {
     let qstring = window.location.search.substring(1);
@@ -44,9 +54,6 @@ function getQueryArgs() {
     }
     return args;
 }
-
-const getEntityId = () => getQueryArgs()['entityId'];
-const getCampaignId = () => getQueryArgs()['campaignId'];
 
 function addChartButtons(rows) {
     for (let row of rows) {
@@ -75,24 +82,63 @@ function addChartButtons(rows) {
     }
 }
 
-function addKeywordButton(keywordBtn) {
-    let entityId = getEntityId();
-    let campaignId = getCampaignId();
-    let adGroupId = $('input[name=adGroupId]')[0].value;
-    let btn = $(`<a href="#" class="${chartClass}">KeywordChart</a>`);
-    btn.click(function(evt) {
-        getKeywordData(getEntityId(), adGroupId, (data) => {
-            // TODO: actually chart something
-        });
+function addCampaignTabs(tabs) {
+    // Add in the analytics tab
+    let a = $('<a href="#">Keyword Analytics</a>');
+    let li = $(`<li class="a-tab-heading ${tabClass}"></li>`);
+    li.append(a);
+    a.click(function(evt) {
+        // Hide all of the body content except the one we want
+        li.addClass('a-active');
+        li.siblings().removeClass('a-active');
+        tabs.parent().children('div').addClass('a-hidden');
+        tabs.parent().find('#ams-unlocked-keyword-analysis').removeClass('a-hidden');
+        $(`#${chartId}`).show();
+        window.setTimeout(() => {
+            let chartData = {
+                mode: 'markers',
+                x: kws.avgCpc,
+                y: kws.clicks,
+                text: kws.kw.map((kw, i) => 
+                    `"${kw}"<br />Impressions: ${kws.impressions[i]}<br />Clicks: ${kws.clicks[i]}<br />Avg CPC: $${kws.avgCpc[i]}<br />Avg COS: ${kws.acos[i]}%`),
+                hoverinfo: 'text',
+                marker: {
+                    sizemode: 'diameter',
+                    size: kws.impressions.map(x => Math.log(x) * 2),
+                    color: kws.acos,
+                    colorscale: [[0, 'rgb(0, 255, 0)'], [0.5, 'rgb(255, 255, 0)'], [1, 'rgb(255, 0, 0)']],
+                },
+            };
+            let layout = {
+                xaxis: {title: 'Average CPC'},
+                yaxis: {title: 'Number of clicks'},
+                margin: {t: 20},
+                height: 600,
+                width: $('.a-box-inner').width(),
+                hovermode: 'closest'
+            };
+            console.log(chartData);
+            Plotly.newPlot(chartId, [chartData], layout, {showLink: false});
+        }, 100);
     });
-    $(keywordBtn).after(btn);
-}
+    $(tabs.children()[0]).after(li);
 
-chrome.runtime.sendMessage({
-    action: 'setSession', 
-    entityId: getEntityId(), 
-    cookies: document.cookie,
-});
+    // Fetch the url we want in order to actually embed it in the page
+    $.ajax({
+        url: chrome.runtime.getURL('keywordAnalytics.html'),
+        success: (data, textStatus, xhr) => {
+            console.log(data);
+            tabs.parent().append(data);
+        },
+    });
+
+    let adGroupId = $('input[name=adGroupId]')[0].value;
+    let kws;
+
+    getKeywordData(getEntityId(), adGroupId, (data) => {
+        kws = transformKeywordData(data);
+    });
+}
 
 function getDataHistory(entityId, campaignId, cb) {
     chrome.runtime.sendMessage({
@@ -173,4 +219,36 @@ function transformHistoryData(data, opt) {
     }
 
     return c;
+}
+
+// TODO: move this stuff up to the server
+function transformKeywordData(data, opt) {
+    let kws = {
+        kw: [],
+        impressions: [],
+        spend: [],
+        clicks: [],
+        acos: [],
+        avgCpc: [],
+    };
+    for (let k of data.aaData) {
+        if (!k.clicks || !parseInt(k.clicks))
+            continue;
+        kws.kw.push(k.keyword);
+        kws.impressions.push(numberize(k.impressions));
+        kws.spend.push(numberize(k.spend));
+        kws.clicks.push(numberize(k.clicks));
+        kws.acos.push(numberize(k.acos));
+        kws.avgCpc.push(numberize(k.avgCpc));
+    }
+
+    return kws;
+}
+
+function numberize(str) {
+    if (!str)
+        return str;
+    str = str.replace(',', '');
+    str = str.replace(' ', '');
+    return parseFloat(str);
 }
