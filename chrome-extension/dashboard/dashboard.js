bb3601f6d688fb0e63e496182c763a343b62d170
Jesse Bangs <jaspax@gmail.com>
2017-05-04 16:23:50 +0300

    added new logic for displaying thumbnail graphs on all measured metrics

--- chrome-extension/dashboard/dashboard.js
+++ chrome-extension/dashboard/dashboard.js
@@ -16,10 +16,11 @@ window.setInterval(() => {
 
     let dashboard = $('#campaignDashboard');
     if (dashboard.find(`#${chartId}`).length == 0) {
-        dashboard.append($(`<div id="${chartId}"></div>`));
+        dashboard.append($(`<div id="${chartId}" style="display:none"></div>`));
     }
 }, 100);
 
+// This dismisses the chart flyout clicking anywhere outside the chart
 function addChartButtons(rows) {
     for (let row of rows) {
         for (let chart of charts) {
@@ -35,9 +36,28 @@ function addChartButtons(rows) {
                 campaignId = campaignId.substring(0, 22); // first 22 chars are the campaignId; timestamp is appended for some reason
                 let btn = $(`<a href="#" class="${chartClass}"><img src="${chartPng}" /></a>`);
                 btn.click(function(evt) {
+                    let newId = chartId+campaignId+chart.config.metric;
+                    let popup = btn.parent().find('#'+newId);
+                    if (!popup.length) {
+                        popup = $('#'+chartId).clone();
+                        popup.attr('id', newId);
+                        popup.addClass(chartId);
+                        let pos = btn.position();
+                        popup.css({top: pos.top + btn.height() + 6, left: pos.left});
+                        btn.after(popup);
+                    }
+                    popup.show();
+
                     getDataHistory(getEntityId(), campaignId, (data) => {
-                        renderChart(data, name, chart);
-                        $('body').scrollTop($('#'+chartId).offset().top);
+                        renderChart(data, name, Object.assign({id: newId}, chart));
+
+                        // Clicking anywhere outside the chart dismisses the chart
+                        $(document).on('click', function() {
+                            if (!$.contains(popup[0], this)) {
+                                popup.hide();
+                                $(document).off('click');
+                            }
+                        });
                     });
                 });
                 $(target).append(btn);
@@ -52,9 +72,7 @@ function getDataHistory(entityId, campaignId, cb) {
         entityId: entityId,
         campaignId: campaignId,
     },
-    (response) => {
-        cb(response.data);
-    });
+    (response) => cb(response.data));
 }
 
 function renderChart(data, name, opt) {
@@ -69,16 +87,13 @@ function renderChart(data, name, opt) {
     };
 
     var layout = {
-      title: `${opt.label} - ${name}`,
-      height: 600,
-      width: $('#campaignDashboard').width(),
+      title: `${opt.label}<br />${name}`,
+      width: 400,
+      height: 300,
       autosize: true,
     };
 
-    Plotly.newPlot(chartId, [series], layout);
-
-    let $chart = $('#'+chartId);
-    $chart.slideDown();
+    Plotly.newPlot(opt.id, [series], layout);
 };
 
 function transformHistoryData(data, opt) {
