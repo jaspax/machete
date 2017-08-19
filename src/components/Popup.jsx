065da30701a24015d7e4ca824f57a760646d5b63
Jesse Bangs <jaspax@gmail.com>
2017-08-19 21:52:38 +0300

    remade the Popup plugin to use react-element-portal and render on the document

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const $ = require('jquery');
 const ga = require('../common/ga.js');
+const ElementPortal = require('react-element-portal').default;
 
 /* For now, we are assuming that the popup is anchored on something which is NOT
  * itself a React element. If this assumption becomes false in the future, we
@@ -12,26 +13,37 @@ const width = 420;
 const gutter = 6;
 const dismissEvent = 'click.machete.popup-dismiss';
 
+let popupCounter = 0;
+
 class Popup extends React.Component {
     render() {
         if (!this.props.show) {
             $(document).off(dismissEvent);
+            if (this.target) {
+                this.target.remove();
+            }
             return null;
         }
 
+        popupCounter++;
+        const popupId = 'machete-popup-' + popupCounter;
+        this.target = $(`<div id="${popupId}" class="machete-popup"></div>`);
+        $(document.body).append(this.target);
+
         const anchor = $('#'+this.props.anchorId);
-        let anchorPos = anchor.position();
+        let anchorPos = anchor.offset();
         let pos = {top: anchorPos.top + anchor.height() + gutter, left: anchorPos.left};
         if (anchorPos.left + width > $(document).width()) { 
             pos = {top: anchorPos.top + anchor.height() + gutter, left: anchorPos.left + anchor.width() - width + gutter};
         }
+        this.target.css(pos);
 
         // Clicking anywhere outside the popup dismisses the chart. We bind the
         // event here, and unbind it when we're actually re-rendered with
         // show=false.
         const self = this;
         $(document).on(dismissEvent, ga.mcatch(function() {
-            if (!$.contains(self.popup, this)) { // eslint-disable-line no-invalid-this
+            if (!$.contains(self.target, this)) { // eslint-disable-line no-invalid-this
                 ga.mga('event', 'popup', 'dismiss');
                 if (self.props.onDismiss) {
                     self.props.onDismiss();
@@ -50,11 +62,9 @@ class Popup extends React.Component {
         */
 
         return (
-            <div className="machete-popup" 
-              ref={ (popup) => this.popup = popup }
-              style={{ top: pos.top, left: pos.left }}>
+            <ElementPortal id={popupId}>
                 {this.props.children}
-            </div>
+            </ElementPortal>
         );
     }
 }
