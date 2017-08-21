cfb9ce9c6476ff6ea416820a43ae8af1d9eebba7
Jesse Bangs <jaspax@gmail.com>
2017-08-21 12:25:41 +0300

    added background ability to gather and store ad group data

--- src/seller-background/index.js
+++ src/seller-background/index.js
@@ -9,6 +9,8 @@ bg.messageListener(function*(req) {
         return yield* bg.getUser();
     if (req.action == 'getCampaignDataRange')
         return yield* getCampaignDataRange(req.campaignId, req.startTimestamp, req.endTimestamp);
+    if (req.action == 'getAdGroupDataRange')
+        return yield* getAdGroupDataRange(req.campaignId, req.adGroupId, req.startTimestamp, req.endTimestamp);
     throw new Error('unknown action');
 });
 
@@ -18,8 +20,18 @@ function* synchronizeCampaignData() {
     try {
         const ranges = yield* getMissingRanges();
         for (const range of ranges) {
-            yield* requestCampaignDataRange(range.start, range.end);
+            const campaignIds = yield* requestCampaignDataRange(range.start, range.end);
+            for (const id of campaignIds) {
+                yield* requestAdGroupDataRange(id, range.start, range.end);
+            }
         }
+    }
+    catch (ex) {
+        /* TURN THIS BACK ON SHORTLY
+        if (ex.status == 401) { // Unauthorized
+            notifyNeedCredentials();
+        } */
+        throw ex;
     }
     finally {
         console.log('synchronizeCampaignData finish at', new Date());
@@ -30,46 +42,75 @@ function* setSession() {
     yield* synchronizeCampaignData();
 }
 
-function* requestCampaignDataRange(startDate, endDate) {
+function* requestAllPages(getCurrentPage) {
+    const pageSize = 50;
+    let currentPage = 0;
+    let totalRecords = 0;
     let data = null;
-    try {
-        console.log('requesting campaign data in range', startDate, endDate);
-        const pageSize = 50;
-        let currentPage = 0;
-        let totalRecords = 0;
-        do {
-            data = yield $.ajax('https://sellercentral.amazon.com/hz/cm/campaign/fetch', {
-                method: 'GET',
-                data: {
-                    sEcho: 1,
-                    parentCreationDate: 0,
-                    iDisplayStart: currentPage,
-                    iDisplayLength: pageSize,
-                    statisticsPeriod: 'CUSTOMIZED',
-                    startDate,
-                    endDate,
-                    aggregates: false,
-                    statusFilter: 'ENABLED'
-                },
-                dataType: 'json',
-            });
 
-            totalRecords = data.iTotalRecords;
-            currentPage++;
-
-            yield* storeCampaignDataRange(data, startDate, endDate);
-        }
-        while (currentPage * pageSize < totalRecords);
+    do {
+        data = yield* getCurrentPage(pageSize, currentPage * pageSize);
+        totalRecords = data.iTotalRecords;
+        currentPage++;
     }
-    catch (ex) {
-        /* TURN THIS BACK ON SHORTLY
-        if (ex.status == 401) { // Unauthorized
-            notifyNeedCredentials();
-        } */
-        throw ex;
-    }
+    while (currentPage * pageSize < totalRecords);
 
     return data;
+}
+
+function* requestCampaignDataRange(startDate, endDate) {
+    let campaignIds = [];
+    yield* requestAllPages(function*(pageSize, currentRecord) {
+        console.log('requesting campaign data in range', startDate, endDate, 'record', currentRecord);
+        const data = yield $.ajax('https://sellercentral.amazon.com/hz/cm/campaign/fetch', {
+            method: 'GET',
+            data: {
+                sEcho: 1,
+                parentCreationDate: 0,
+                iDisplayStart: currentRecord,
+                iDisplayLength: pageSize,
+                statisticsPeriod: 'CUSTOMIZED',
+                startDate,
+                endDate,
+                aggregates: false,
+                statusFilter: 'ENABLED'
+            },
+            dataType: 'json',
+        });
+        yield* storeCampaignDataRange(data, startDate, endDate);
+
+        // Gather the IDs of the campaigns discovered in this data range
+        const idIndex = data.columnNames.indexOf('id');
+        campaignIds = campaignIds.concat(data.aaData.map(x => x[idIndex]).filter(x => x));
+
+        return data;
+    });
+
+    return campaignIds;
+}
+
+function* requestAdGroupDataRange(campaignId, startDate, endDate) {
+    return yield* requestAllPages(function*(pageSize, currentRecord) {
+        console.log('requesting adGroupData data in range', startDate, endDate, 'record', currentRecord);
+        const data = yield $.ajax('https://sellercentral.amazon.com/hz/cm/adgroup/fetch', {
+            method: 'GET',
+            data: {
+                sEcho: 1,
+                parentCreationDate: 0,
+                iDisplayStart: currentRecord,
+                iDisplayLength: pageSize,
+                statisticsPeriod: 'CUSTOMIZED',
+                startDate,
+                endDate,
+                aggregates: false,
+                statusFilter: 'ENABLED',
+                filters: JSON.stringify({campaign: { id: { eq: [campaignId] } } }),
+            },
+            dataType: 'json',
+        });
+        yield* storeAdGroupDataRange(data, startDate, endDate);
+        return data;
+    });
 }
 
 function* getMissingRanges() {
@@ -89,6 +130,21 @@ function* storeCampaignDataRange(data, startDate, endDate) {
 
 function* getCampaignDataRange(campaignId, startTimestamp, endTimestamp) {
     return yield $.ajax(`https://${constants.hostname}/api/seller/campaignData/${campaignId}/${startTimestamp}-${endTimestamp}`, {
+        method: 'GET',
+        dataType: 'json'
+    });
+}
+
+function* storeAdGroupDataRange(data, startDate, endDate) {
+    return yield $.ajax(`https://${constants.hostname}/api/seller/adGroupData/${startDate}-${endDate}`, {
+        method: 'PUT',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+    });
+}
+
+function* getAdGroupDataRange(campaignId, adGroupId, startTimestamp, endTimestamp) {
+    return yield $.ajax(`https://${constants.hostname}/api/seller/adGroupData/${campaignId}/${adGroupId}/${startTimestamp}-${endTimestamp}`, {
         method: 'GET',
         dataType: 'json'
     });
