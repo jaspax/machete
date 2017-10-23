7b66d845a0f659515fff2895af7ba050fee2bed3
Jesse Bangs <jaspax@gmail.com>
2017-10-23 07:49:54 +0300

    added a granularity selector to all history views

--- src/components/TimeSeriesGranularitySelector.jsx
+++ src/components/TimeSeriesGranularitySelector.jsx
@@ -4,19 +4,21 @@ const PropTypes = require('prop-types');
 const Select = require('react-select').default;
 
 const options = [
-    { label: 'Day', value: 0, chunk: 'day' },
-    { label: 'Week', value: 1, chunk: 'week' },
-    { label: 'Month', value: 2, chunk: 'month' },
+    { label: 'Daily', value: 0, chunk: 'day' },
+    { label: 'Weekly', value: 1, chunk: 'week' },
+    { label: 'Monthly', value: 2, chunk: 'month' },
 ];
 
 function TimeSeriesGranularitySelector(props) {
     const selected = options.find(x => x.chunk == props.value);
     const value = selected ? selected.value : 0;
-    return <Select name="granularity-select"
-        options={options}
-        onChange={(selected) => props.onChange(selected.chunk)}
-        value={value}
-    />;
+    return <div className="machete-granularity-select-wrapper">
+        <b>Display statistics:&nbsp;</b>
+        <Select name="granularity-select" className="machete-granularity-select"
+            options={options}
+            onChange={(selected) => props.onChange(selected.chunk)}
+            value={value} />
+    </div>;
 }
 
 TimeSeriesGranularitySelector.options = options;
