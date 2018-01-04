f7a7ad7cdc979015587c1289c3c3f02138d83829
Jesse Bangs <jaspax@gmail.com>
2018-01-04 11:13:40 +0200

    moved the granularity selector into a horizontal group and fixed up styling and labeling

--- src/components/MetricSelector.jsx
+++ src/components/MetricSelector.jsx
@@ -4,13 +4,16 @@ const PropTypes = require('prop-types');
 const { RadioGroup, Radio } = require('react-radio-group');
 
 function MetricSelector(props) {
-    return <RadioGroup className="machete-radio-horizontal" name="metricSelection" selectedValue={props.selected} onChange={props.onChange}>
-        <Radio id="metric-selector-all" value="all" /><label htmlFor="metric-selector-all">All</label>
-        <Radio id="metric-selector-impressions" value="impressions" /><label htmlFor="metric-selector-impressions">Impressions</label>
-        <Radio id="metric-selector-clicks" value="clicks" /><label htmlFor="metric-selector-clicks">Clicks</label>
-        <Radio id="metric-selector-sales" value="sales" /><label htmlFor="metric-selector-sales">Sales</label>
-        <Radio id="metric-selector-spend" value="spend" /><label htmlFor="metric-selector-spend">Spend</label>
-    </RadioGroup>;
+    return <div className="machete-radio-group-wrapper">
+        <div className="machete-radio-group-label">Focus metric:</div>
+        <RadioGroup className="machete-radio-horizontal" name="metricSelection" selectedValue={props.selected} onChange={props.onChange}>
+            <Radio id="metric-selector-all" value="all" /><label htmlFor="metric-selector-all">All</label>
+            <Radio id="metric-selector-impressions" value="impressions" /><label htmlFor="metric-selector-impressions">Impressions</label>
+            <Radio id="metric-selector-clicks" value="clicks" /><label htmlFor="metric-selector-clicks">Clicks</label>
+            <Radio id="metric-selector-sales" value="sales" /><label htmlFor="metric-selector-sales">Sales</label>
+            <Radio id="metric-selector-spend" value="spend" /><label htmlFor="metric-selector-spend">Spend</label>
+        </RadioGroup>
+    </div>;
 }
 
 MetricSelector.propTypes = {
--- src/components/TimeSeriesGranularitySelector.jsx
+++ src/components/TimeSeriesGranularitySelector.jsx
@@ -1,27 +1,19 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-const Select = require('react-select').default;
-
-const options = [
-    { label: 'Daily', value: 0, chunk: 'day' },
-    { label: 'Weekly', value: 1, chunk: 'week' },
-    { label: 'Monthly', value: 2, chunk: 'month' },
-];
+const { RadioGroup, Radio } = require('react-radio-group');
 
 function TimeSeriesGranularitySelector(props) {
-    const selected = options.find(x => x.chunk == props.value);
-    const value = selected ? selected.value : 0;
-    return <div className="machete-granularity-select-wrapper">
-        <b>Display statistics:&nbsp;</b>
-        <Select name="granularity-select" className="machete-granularity-select"
-            options={options}
-            onChange={(selected) => props.onChange(selected.chunk)}
-            value={value} />
+    return <div className="machete-radio-group-wrapper">
+        <div className="machete-radio-group-label">Display statistics:</div>
+        <RadioGroup className="machete-radio-horizontal" id="machete-granularity-selector" selectedValue={props.value} onChange={props.onChange}>
+            <Radio id="machete-granularity-selector-daily" value="day" /><label htmlFor="machete-granularity-selector-daily">Daily</label>
+            <Radio id="machete-granularity-selector-weekly" value="week" /><label htmlFor="machete-granularity-selector-weekly">Weekly</label>
+            <Radio id="machete-granularity-selector-monthly" value="month" /><label htmlFor="machete-granularity-selector-monthly">Monthly</label>
+        </RadioGroup>
     </div>;
 }
 
-TimeSeriesGranularitySelector.options = options;
 TimeSeriesGranularitySelector.propTypes = { 
     value: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired
