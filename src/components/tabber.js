d23bb1245777bdd496e5e5c000d66bb83a99bef8
Jesse Bangs <jaspax@gmail.com>
2018-01-05 15:21:45 +0200

    Revert "got the tab memory working, though the back button doesn't work"
    This reverts commit 016fc6717d65a3bf467a518b37ebc55f42873174, because it caused
    all sorts of problems, especially on the seller page.

--- src/components/tabber.js
+++ src/components/tabber.js
@@ -2,52 +2,35 @@ const $ = require('jquery');
 const ga = require('../common/ga.js');
 
 function tabber(tabs, opt) {
+    let hasActivated = false;
     let tab = $(`<li className="a-tab-heading"><a href="#" >${opt.label}</a></li>`);
     let container = $(`<div class="a-box a-box-tab a-tab-content a-hidden" data-a-name="machete-${opt.label}"></div>`);
+    if (opt.active) {
+        tab.addClass('a-active');
+        container.removeClass('a-hidden');
+    }
+
+    tab.find('a').click(ga.mcatch((evt) => {
+        evt.preventDefault();
+        ga.mga('event', 'kword-data-tab', 'activate', opt.label);
+
+        tab.addClass('a-active');
+        tab.siblings().removeClass('a-active');
+        tabs.parent().children('div').addClass('a-hidden');
+        container.removeClass('a-hidden');
+
+        if (opt.activate && !hasActivated) {
+            hasActivated = true;
+            window.setTimeout(ga.mcatch(() => opt.activate(container)));
+        }
+    }));
 
     if (opt.insertIndex)
         $(tabs.children()[opt.insertIndex - 1]).after(tab);
     else
         tabs.append(tab);
     tabs.parent().append(container);
-
-    if (opt.active || isActiveTab(opt.label)) {
-        activate({ evt: null, tabs, tab, container, opt });
-    }
-    tab.find('a').click(ga.mcatch((evt) => activate({ evt, tabs, tab, container, opt })));
-
     return { tab, container };
-}
-
-function activate(arg) {
-    const { evt, tabs, tab, container, opt } = arg;
-    if (evt)
-        evt.preventDefault();
-
-    ga.mga('event', 'kword-data-tab', 'activate', opt.label);
-
-    tab.addClass('a-active');
-    tab.siblings().removeClass('a-active');
-    tabs.parent().children('div').addClass('a-hidden');
-    container.removeClass('a-hidden');
-
-    window.history.pushState({ activeTab: opt.label }, '', getTabUrl(opt.label));
-
-    if (opt.activate && !opt.hasActivated) {
-        opt.hasActivated = true;
-        window.setTimeout(ga.mcatch(() => opt.activate(container)));
-    }
-}
-
-function getTabUrl(label, location = window.location.href) {
-    const url = new URL(location);
-    url.searchParams.set('macheteTab', label);
-    return url.toString();
-}
-
-function isActiveTab(label, location = window.location.href) {
-    const url = new URL(location);
-    return url.searchParams.get('macheteTab') == label;
 }
 
 module.exports = tabber;
