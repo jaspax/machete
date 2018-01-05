80c4027c7b895d2e9029b07718eaef9f4265643a
Jesse Bangs <jaspax@gmail.com>
2018-01-05 13:32:01 +0200

    made the loading message on the bubble chart disappear when it should

--- src/components/KeywordBubbleChart.jsx
+++ src/components/KeywordBubbleChart.jsx
@@ -12,6 +12,7 @@ class KeywordBubbleChart extends React.Component {
         super(props);
         chartCounter++;
         this.id = "KeywordBubbleChart" + chartCounter;
+        this.loadingId = this.id + 'Loading';
     }
 
     render() {
@@ -19,7 +20,9 @@ class KeywordBubbleChart extends React.Component {
             width: this.props.width,
             height: this.props.height,
         };
-        return <div style={chartStyle} className="loading-large" id={this.id}>Preparing keyword chart...</div>;
+        return <div style={chartStyle} className="loading-large" id={this.id}>
+            <span id={this.loadingId}>Preparing keyword chart...</span>
+        </div>;
     }
 
     shouldComponentUpdate() {
@@ -39,32 +42,43 @@ class KeywordBubbleChart extends React.Component {
     }
 
     renderChart() {
-        let kws = this.props.keywordData;
-        let chartData = {
-            mode: 'markers',
-            x: kws.impressions,
-            y: kws.clicks,
-            text: kws.kw.map(ga.mcatch((kw, i) =>
-                `"${kw}"<br />Impressions: ${kws.impressions[i]}<br />Clicks: ${kws.clicks[i]}<br />Avg CPC: ${common.moneyFmt(kws.avgCpc[i])}<br />ACOS: ${common.pctFmt(kws.acos[i])}`)),
-            hoverinfo: 'text',
-            marker: {
-                sizemode: 'area',
-                sizeref: Math.max.apply(null, kws.spend) / 2000,
-                size: kws.spend,
-                color: kws.acos,
-                colorscale: [[0, 'rgb(0, 255, 0)'], [0.5, 'rgb(255, 255, 0)'], [1, 'rgb(255, 0, 0)']],
-            },
-        };
-        let layout = {
-            xaxis: {title: 'Impressions'},
-            yaxis: {title: 'Clicks'},
-            margin: { l: 40, r: 20, b: 40, t: 20, pad: 4 },
-            height: this.props.height,
-            width: this.props.width,
-            hovermode: 'closest',
-            showlegend: false,
-        };
-        window.setTimeout(ga.mcatch(() => Plotly.newPlot(this.id, [chartData], layout, {showLink: false})));
+        window.setTimeout(ga.mcatch(() => {
+            let kws = this.props.keywordData;
+            let chartData = {
+                mode: 'markers',
+                x: kws.impressions,
+                y: kws.clicks,
+                text: kws.kw.map(ga.mcatch((kw, i) =>
+                    `"${kw}"<br />Impressions: ${kws.impressions[i]}<br />Clicks: ${kws.clicks[i]}<br />Avg CPC: ${common.moneyFmt(kws.avgCpc[i])}<br />ACOS: ${common.pctFmt(kws.acos[i])}`)),
+                hoverinfo: 'text',
+                marker: {
+                    sizemode: 'area',
+                    sizeref: Math.max.apply(null, kws.spend) / 2000,
+                    size: kws.spend,
+                    color: kws.acos,
+                    colorscale: [[0, 'rgb(0, 255, 0)'], [0.5, 'rgb(255, 255, 0)'], [1, 'rgb(255, 0, 0)']],
+                },
+            };
+            let layout = {
+                xaxis: {title: 'Impressions'},
+                yaxis: {title: 'Clicks'},
+                margin: { l: 40, r: 20, b: 40, t: 20, pad: 4 },
+                height: this.props.height,
+                width: this.props.width,
+                hovermode: 'closest',
+                showlegend: false,
+            };
+            Plotly.newPlot(this.id, [chartData], layout, {showLink: false});
+
+            const target = document.getElementById(this.id);
+            if (target && target.on) {
+                target.on('plotly_afterplot', () => {
+                    const loading = document.getElementById(this.loadingId);
+                    if (loading)
+                        loading.parentNode.removeChild(loading);
+                });
+            }
+        }));
     }
 }
 
