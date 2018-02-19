11281f894e9cc0436332e2790089be899d5f21a0
Jesse Bangs <jaspax@gmail.com>
2018-02-19 11:21:38 +0200

    remove html from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,6 @@ module.exports = function(grunt) {
         copy: {
             css: { expand: true, src: 'css/**', dest: `out/${product}`, },
             img: { expand: true, src: 'images/**', dest: `out/${product}`, },
-            html: { expand: true, flatten: true, src: `html/${product}/**`, dest: `out/${product}/html`, },
             datepickerCss: { src: 'node_modules/react-datepicker/dist/react-datepicker.css', dest: `out/${product}/css/react-datepicker.css` },
             tableCss: { src: 'node_modules/react-table/react-table.css', dest: `out/${product}/css/react-table.css` },
             selectCss: { src: 'node_modules/react-select/dist/react-select.min.css', dest: `out/${product}/css/react-select.css` },
