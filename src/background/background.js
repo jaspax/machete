6375ab357c02088f8f0493cae1f7c0abed43f526
Jesse Bangs <jaspax@gmail.com>
2018-02-27 09:00:33 +0200

    correctly store lifetime data snapshots as such rather than storing them as daily data

--- src/background/background.js
+++ src/background/background.js
@@ -167,7 +167,7 @@ function* requestCampaignData(entityId) {
             },
             dataType: 'json',
         });
-        yield* storeDataCloud(entityId, Date.now(), data);
+        yield* storeLifetimeCampaignData(entityId, Date.now(), data);
     }
 
     let earliestData = null;
@@ -186,7 +186,7 @@ function* requestCampaignData(entityId) {
         if (!earliestData)
             earliestData = data;
 
-        yield* storeDataCloud(entityId, date, data);
+        yield* storeDailyCampaignData(entityId, date, data);
     });
 
     let timestamp = Date.now();
@@ -242,8 +242,16 @@ function* requestKeywordData(entityId, adGroupId) {
     yield* storeKeywordDataCloud(entityId, adGroupId, timestamp, data);
 }
 
-function* storeDataCloud(entityId, timestamp, data) {
+function* storeDailyCampaignData(entityId, timestamp, data) {
     return yield bg.ajax(`${bg.serviceUrl}/api/campaignData/${entityId}?timestamp=${timestamp}`, {
+        method: 'PUT',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+    });
+}
+
+function* storeLifetimeCampaignData(entityId, timestamp, data) {
+    return yield bg.ajax(`${bg.serviceUrl}/api/data/${entityId}?timestamp=${timestamp}`, {
         method: 'PUT',
         data: JSON.stringify(data),
         contentType: 'application/json',
