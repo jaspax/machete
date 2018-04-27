a9611c3d14cbc525f2ee5a94d685ab286873acd5
Jesse Bangs <jaspax@gmail.com>
2018-04-27 15:17:38 +0300

    parallelize kdp data download and added client-side data parsing

--- src/background/kdp.js
+++ src/background/kdp.js
@@ -1,14 +1,16 @@
 const bg = require('./common.js');
 const moment = require('moment');
+const _ = require('lodash');
 
 function* setSession() {
-    const asins = yield* fetchAsins();
-    for (const asin of asins) {
-        const sales = yield fetchSalesData(asin);
-        const ku = yield fetchKuData(asin);
+    const time = Date.now();
+    const asins = yield* fetchAsins(time);
+    yield bg.parallelQueue(asins, function* (asin) {
+        const sales = yield* fetchSalesData(time, asin);
+        const ku = yield* fetchKuData(time, asin);
         
         console.log('asin', asin, 'sales', sales, 'ku', ku);
-    }
+    });
 }
 
 function requestPermission() {
@@ -21,7 +23,7 @@ function requestPermission() {
     });
 }
 
-function baseRequest() {
+function baseRequest(time) {
     const todayFmt = moment().format('YYYY-MM-DD');
     const past90DaysFmt = moment().subtract(90, 'days').format('YYYY-MM-DD');
     return {
@@ -31,7 +33,7 @@ function baseRequest() {
         namespace: 'kdp',
         pageid: 'KDP_UI_OP',
         vendorcode: '',
-        time: Date.now(),
+        time,
         _filter_marketplaceId: JSON.stringify({ "type": "dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
         _filter_author: JSON.stringify({ "type": "dynamic-dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
         _filter_asin: JSON.stringify({ "type": "dynamic-dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
@@ -49,8 +51,8 @@ function kdpAjax(request) {
     });
 }
 
-function* fetchAsins() {
-    const titleRequest = Object.assign(baseRequest(), { 
+function* fetchAsins(time) {
+    const titleRequest = Object.assign(baseRequest(time), { 
         'post-ajax': JSON.stringify([{ action: "load", ids: ["sales-dashboard-chart-orders", "sales-dashboard-chart-ku", "sales-dashboard-table"], type: "onLoad" }]),
         target: JSON.stringify([{ id: "sales-dashboard-dd-asin", type: "dynamic-dropdown", metadata: "STRING"}]),
         'request-id': 'KDPGetTitles_OP'
@@ -61,31 +63,30 @@ function* fetchAsins() {
     return data['dynamic-dropdown'].map(x => x[0].split(','));
 }
 
-function fetchSalesData(asin) {
-    const reportRequest = Object.assign(baseRequest(), {
+function* fetchSalesData(time, asin) {
+    const reportRequest = Object.assign(baseRequest(time), {
         'post-ajax': JSON.stringify([{ "action": "show", "ids": ["sales-dashboard-export-button"], "type": "onLoad" }]),
         target: JSON.stringify([{ "id": "sales-dashboard-chart-orders", "type": "chart", "metadata": "DATE" }]),
         'request-id': 'KDPGetLineChart_OP',
         _filter_asin: JSON.stringify({ "type": "dynamic-dropdown", "value": asin }), // eslint-disable-line camelcase
     });
     
-    return kdpAjax(reportRequest);
+    const response = yield kdpAjax(reportRequest);
+    const data = JSON.parse(response.data);
+    return _.zip(data.chart.xvalues, data.chart.line1);
 }
 
-function fetchKuData(asin) {
-    const reportRequest = Object.assign(baseRequest(), {
+function* fetchKuData(time, asin) {
+    const reportRequest = Object.assign(baseRequest(time), {
         'post-ajax': [],
         target: JSON.stringify([{ "id": "sales-dashboard-chart-ku", "type": "chart", "metadata": "DATE" }]),
         'request-id': 'KDPGetLineChartKU_OP',
         _filter_asin: JSON.stringify({ "type": "dynamic-dropdown", "value": asin }), // eslint-disable-line camelcase
     });
 
-    return bg.ajax({
-        url: 'https://kdp.amazon.com/en_US/reports-new/data',
-        method: 'POST',
-        data: reportRequest,
-        dataType: 'json',
-    });
+    const response = yield kdpAjax(reportRequest);
+    const data = JSON.parse(response.data);
+    return _.zip(data.chart.xvalues, data.chart.line1);
 }
 
 module.exports = {
