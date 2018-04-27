0f634180313f5855714b9227e87d8c43825090a1
Jesse Bangs <jaspax@gmail.com>
2018-04-27 14:59:05 +0300

    filled out KDP data-scraping functions enough to dump them into the log

--- src/background/kdp.js
+++ src/background/kdp.js
@@ -1,18 +1,14 @@
 const bg = require('./common.js');
 const moment = require('moment');
 
-/*
-const co = require('co');
-const common = require('../common/common.js');
-const constants = require('../common/constants.js');
-const ga = require('../common/ga.js');
-*/
-
 function* setSession() {
-    const sales = yield fetchSalesData();
-    const ku = yield fetchKuData();
-
-    console.log('sales', sales, 'KU', ku);
+    const asins = yield* fetchAsins();
+    for (const asin of asins) {
+        const sales = yield fetchSalesData(asin);
+        const ku = yield fetchKuData(asin);
+        
+        console.log('asin', asin, 'sales', sales, 'ku', ku);
+    }
 }
 
 function requestPermission() {
@@ -39,32 +35,50 @@ function baseRequest() {
         _filter_marketplaceId: JSON.stringify({ "type": "dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
         _filter_author: JSON.stringify({ "type": "dynamic-dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
         _filter_asin: JSON.stringify({ "type": "dynamic-dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
-        _filter_reportDate: JSON.stringify({ "type": "date-range", "from": past90DaysFmt, "to": todayFmt}), // eslint-disable-line camelcase
         _filter_book_type: JSON.stringify({ "type": "dropdown", "value": ["_ALL"] }), // eslint-disable-line camelcase
+        _filter_reportDate: JSON.stringify({ "type": "date-range", "from": past90DaysFmt, "to": todayFmt}), // eslint-disable-line camelcase
     };
 }
 
-function fetchSalesData() {
-    const reportRequest = Object.assign({
-        'post-ajax': JSON.stringify([{ "action": "show", "ids": ["sales-dashboard-export-button"], "type": "onLoad" }]),
-        target: JSON.stringify([{ "id": "sales-dashboard-chart-orders", "type": "chart", "metadata": "DATE" }]),
-        'request-id': 'KDPGetLineChart_OP',
-    }, baseRequest());
-    
+function kdpAjax(request) {
     return bg.ajax({
         url: 'https://kdp.amazon.com/en_US/reports-new/data',
         method: 'POST',
-        data: reportRequest,
+        data: request,
         dataType: 'json',
     });
 }
 
-function fetchKuData() {
-    const reportRequest = Object.assign({
+function* fetchAsins() {
+    const titleRequest = Object.assign(baseRequest(), { 
+        'post-ajax': JSON.stringify([{ action: "load", ids: ["sales-dashboard-chart-orders", "sales-dashboard-chart-ku", "sales-dashboard-table"], type: "onLoad" }]),
+        target: JSON.stringify([{ id: "sales-dashboard-dd-asin", type: "dynamic-dropdown", metadata: "STRING"}]),
+        'request-id': 'KDPGetTitles_OP'
+    });
+
+    const response = yield kdpAjax(titleRequest);
+    const data = JSON.parse(response.data);
+    return data['dynamic-dropdown'].map(x => x[0].split(','));
+}
+
+function fetchSalesData(asin) {
+    const reportRequest = Object.assign(baseRequest(), {
+        'post-ajax': JSON.stringify([{ "action": "show", "ids": ["sales-dashboard-export-button"], "type": "onLoad" }]),
+        target: JSON.stringify([{ "id": "sales-dashboard-chart-orders", "type": "chart", "metadata": "DATE" }]),
+        'request-id': 'KDPGetLineChart_OP',
+        _filter_asin: JSON.stringify({ "type": "dynamic-dropdown", "value": asin }), // eslint-disable-line camelcase
+    });
+    
+    return kdpAjax(reportRequest);
+}
+
+function fetchKuData(asin) {
+    const reportRequest = Object.assign(baseRequest(), {
         'post-ajax': [],
         target: JSON.stringify([{ "id": "sales-dashboard-chart-ku", "type": "chart", "metadata": "DATE" }]),
         'request-id': 'KDPGetLineChartKU_OP',
-    }, baseRequest());
+        _filter_asin: JSON.stringify({ "type": "dynamic-dropdown", "value": asin }), // eslint-disable-line camelcase
+    });
 
     return bg.ajax({
         url: 'https://kdp.amazon.com/en_US/reports-new/data',
