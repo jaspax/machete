8ed2623a39d95563e508d6f266eb889f7398fb54
JS Bangs <jaspax@gmail.com>
2019-02-25 15:17:53 +0200

    use the correct campaignId field for the rta collector

--- src/background/sp-rta.js
+++ src/background/sp-rta.js
@@ -20,7 +20,7 @@ module.exports = function(domain, entityId) {
         if (!campaigns.length)
             return true; // I guess?
 
-        const campaign = campaigns.find(x => x.id.slice(0, 2) != 'AC');
+        const campaign = campaigns.find(x => x.campaignId.slice(0, 2) != 'AC');
         if (campaign) {
             await bg.ajax(`https://${domain}/rta/campaign/?entityId=${entityId}&campaignId=${campaign.campaignId}`, {
                 method: 'GET',
