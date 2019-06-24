62b15b172d1a1e3fffeda042adb9e06ecf239200
JS Bangs <jaspax@gmail.com>
2019-06-24 21:49:35 +0300

    remove unused things from background/sp

--- src/background/index.js
+++ src/background/index.js
@@ -1,35 +1,13 @@
 const bg = require('./common.js');
 const sp = require('./sp.js');
-const seller = require('./seller.js');
 const kdp = require('./kdp.js');
 
 bg.messageListener(function(req) { // eslint-disable-line complexity
     switch (req.action) {
         // Valid for every page
-        case 'getUser':
-            return bg.getUser(req);
-        case 'startSession':
-            return bg.startSession(req);
         case 'sayHello':
             return bg.sayHello(req);
 
-        // AMS actions
-        case 'sp.getAllowedCampaigns': 
-            return sp.getAllowedCampaigns(req);
-        case 'sp.getCampaignSummaries': 
-            return sp.getCampaignSummaries(req);
-        case 'sp.getAllCampaignData':
-            return sp.getAllCampaignData(req);
-        case 'sp.getDataHistory':
-            return sp.getDataHistory(req);
-        case 'sp.getAggregateCampaignHistory':
-            return sp.getAggregateCampaignHistory(req);
-        case 'sp.getKeywordData':
-            return sp.getKeywordData(req);
-        case 'sp.getAggregateKeywordData':
-            return sp.getAggregateKeywordData(req);
-        case 'sp.storeAdGroupMetadata':
-            return sp.storeAdGroupMetadata(req);
         case 'sp.setBrandName':
             return sp.setBrandName(req);
 
@@ -66,21 +44,6 @@ bg.messageListener(function(req) { // eslint-disable-line complexity
             return kdp.requestSalesData(req);
         case 'kdp.requestKuData':
             return kdp.requestKuData(req);
-
-
-        // Seller actions
-        case 'seller.getSummaries':
-            return seller.getSummaries(req);
-        case 'seller.getCampaignDataRange':
-            return seller.getCampaignDataRange(req);
-        case 'seller.getAdGroupDataRange':
-            return seller.getAdGroupDataRange(req);
-        case 'seller.getAdDataRange':
-            return seller.getAdDataRange(req);
-        case 'seller.getAdDataRangeByAsin':
-            return seller.getAdDataRangeByAsin(req);
-        case 'seller.getKeywordDataRange':
-            return seller.getKeywordDataRange(req);
 
         default:
             throw new Error('unknown action: ' + req.action);
--- src/background/sp.js
+++ src/background/sp.js
@@ -1,46 +1,11 @@
 const bg = require('./common.js');
 const _ = require('lodash');
 const common = require('../common/common.js');
-const constants = require('../common/constants.js');
 const ga = require('../common/ga.js');
 const spData = require('../common/sp-data.js');
 
 const spCm = require('./sp-cm.js');
 const spRta = require('./sp-rta.js');
-
-const entityDailySyncEvent = {};
-
-function checkEntityId(entityId) {
-    if (bg.isUnset(entityId)) {
-        throw new Error('invalid entityId=' + entityId);
-    }
-}
-
-function mkEvent() {
-    const event = {};
-    event.promise = new Promise((resolve, reject) => {
-        event.set = val => {
-            resolve(val);
-            event.set = () => { /* ignore multiple sets */ };
-        };
-        event.error = e => {
-            reject(e);
-            event.error = () => { /* ignore multiple errors */ };
-        };
-    });
-
-    return event;
-}
-
-function getEntitySyncEvent(entityId) {
-    if (!entityDailySyncEvent[entityId]) {
-        const event = mkEvent(`entity ${entityId}`);
-        entityDailySyncEvent[entityId] = event;
-        if (bg.hasSyncedToday('sp'))
-            event.set();
-    }
-    return entityDailySyncEvent[entityId];
-}
 
 const collectorCache = {};
 
@@ -88,110 +53,6 @@ function getCollector(domain, entityId) {
     return getCollectorImpl(domain, entityId, 'general', c => c.probe());
 }
 
-async function dataGather(req) {
-    ga.beginLogBuffer('sp.dataGather');
-    // We want to make sure that we at least attempt to sync every single
-    // domain, but any exceptions we encounter should be propagated so that we
-    // don't record this as a success.
-    let deferredException = null;
-    
-    let entities = bg.getEntityIds();
-
-    // if we are responding to a client request, always sync that entityId first
-    if (req && req.entityId) {
-        entities.unshift({ domain: req.domain, entityId: req.entityId });
-        entities = _.uniqBy(entities, x => x.entityId);
-    }
-
-    for (const { domain, entityId } of entities) {
-        if (bg.isUnset(entityId))
-            continue;
-
-        try {
-            const collector = await getCollector(domain, entityId);
-
-            // Store entity metadata locally if we have all relevant fields
-            const entityMetadata = bg.setEntityId(entityId, { collector: collector.name });
-            if (entityMetadata.name && entityMetadata.collector && entityMetadata.domain) {
-                await storeEntityMetadata(entityMetadata);
-            }
-
-            const campaignIds = await requestCampaignData(collector);
-            const adGroups = await getAdGroups(entityId);
-            const summaries = await getCampaignSummaries({ entityId });
-
-            await bg.parallelQueue(campaignIds, async function(campaignId) {
-                campaignId = spData.stripPrefix(campaignId);
-                const summary = summaries.find(x => x.campaignId == campaignId);
-                if (!spData.isRunning(summary)) {
-                    return;
-                }
-
-                const adGroupItem = adGroups.find(x => x.campaignId == campaignId);
-                let adGroupId = null;
-                if (adGroupItem) {
-                    adGroupId = adGroupItem.adGroupId;
-                }
-                else {
-                    console.log('requesting adGroupId for', collector.entityId, campaignId);
-                    adGroupId = await collector.getAdGroupId(campaignId);
-                    if (adGroupId)
-                        await storeAdGroupMetadata({ entityId: collector.entityId, adGroupId, campaignId });
-                }
-
-                if (adGroupId) {
-                    if (!(summary && summary.asin)) {
-                        await requestCampaignMetadata(collector, campaignId, spData.stripPrefix(adGroupId));
-                    }
-                    await requestKeywordData({ entity: { entityId, domain }, campaignId, adGroupId: spData.stripPrefix(adGroupId) });
-                }
-            });
-        }
-        catch (ex) {
-            if (!bg.handleServerErrors(ex, "sp.dataGather")) {
-                ga.merror(ex, `context: domain ${domain}, entityId ${entityId}`);
-            }
-            deferredException = ex;
-        }
-    }
-
-    ga.endLogBuffer();
-    if (deferredException) {
-        throw deferredException;
-    }
-}
-
-const getAllowedCampaigns = bg.cache.coMemo(async function({ entityId }) {
-    checkEntityId(entityId);
-    const allowed = await bg.ajax(`${bg.serviceUrl}/api/data/${entityId}/allowed`, { 
-        method: 'GET',
-        responseType: 'json'
-    });
-
-    if (allowed.length)
-        return allowed;
-
-    return bg.ajax(`${bg.serviceUrl}/api/data/${entityId}/allowed`, { 
-        method: 'GET',
-        responseType: 'json'
-    });
-}, { maxAge: 30000 });
-
-const getCampaignSummaries = bg.cache.coMemo(async function({ entityId }) {
-    checkEntityId(entityId);
-
-    await summaryReady(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/data/${entityId}/summary`, { 
-        method: 'GET',
-        responseType: 'json'
-    });
-}, { maxAge: 30000 });
-
-async function summaryReady(entityId) {
-    const syncEvent = getEntitySyncEvent(entityId);
-    await syncEvent.promise;
-}
-
 async function requestLifetimeCampaignData({ entity }) {
     const collector = await getCollector(entity.domain, entity.entityId);
     return collector.getLifetimeCampaignData();
@@ -220,234 +81,6 @@ async function requestKeywordData({ entity, campaignId, adGroupId }) {
 function requestAdEntities({ domain }) {
     const collector = spCm(domain);
     return collector.getAdEntities();
-}
-
-async function requestCampaignData(collector) {
-    const syncEvent = getEntitySyncEvent(collector.entityId);
-    let campaignIds = [];
-
-    try {
-        console.log('requesting campaign data for', collector.entityId);
-        const missing = await getMissingDates(collector.entityId);
-        const days = new Set(missing.missingDays);
-        const now = Date.now();
-
-        if (missing.needLifetime) {
-            const data = await collector.getLifetimeCampaignData();
-            await storeLifetimeCampaignData(collector.entityId, now, data);
-        }
-
-        if (days.size) {
-            const latestDay = Math.max(...days.values());
-            const latestData = await collector.getDailyCampaignData(latestDay);
-            await storeDailyCampaignData(collector.entityId, latestDay, latestData);
-
-            campaignIds = latestData.map(x => x.campaignId);
-            const status = await collector.getCampaignStatus(campaignIds);
-            await storeCampaignStatus(collector.entityId, now, status);
-            syncEvent.set();
-
-            days.delete(latestDay);
-            await bg.parallelQueue(days.values(), async date => {
-                const data = await collector.getDailyCampaignData(date);
-                await storeDailyCampaignData(collector.entityId, date, data);
-            });
-        }
-    }
-    finally {
-        syncEvent.set();
-    }
-
-    return campaignIds;
-}
-
-async function requestCampaignMetadata(collector, campaignId, adGroupId) {
-    const asin = await collector.getCampaignAsin(campaignId, adGroupId);
-    if (asin) {
-        await storeCampaignMetadata(collector.entityId, campaignId, asin);
-    }
-}
-
-async function requestKeywordDataImpl(collector, campaignId, adGroupId) {
-    if (!(campaignId && adGroupId)) {
-        ga.merror(`missing campaignId or adGroupId: entityId ${collector.entityId}, campaignId ${campaignId}, adGroupId ${adGroupId}, collector ${collector.name}`);
-        return;
-    }
-
-    if (campaignId[0] == 'C') {
-        // this is a product display ad, which doesn't have keywords. Skip it.
-        console.log('Skipping requestKeywordData for', campaignId, adGroupId, 'because of wrong ad type');
-        return;
-    }
-
-    console.log('requesting keyword data for', collector.entityId, campaignId, adGroupId);
-    const data = await collector.getKeywordData(campaignId, adGroupId);
-
-    if (data && data.length) {
-        await storeKeywordData(collector.entityId, adGroupId, Date.now(), data);
-    }
-}
-
-function storeDailyCampaignData(entityId, timestamp, data) {
-    if (!(data && data.length))
-        return Promise.resolve();
-
-    return bg.ajax(`${bg.serviceUrl}/api/campaignData/${entityId}?timestamp=${timestamp}`, {
-        method: 'PUT',
-        jsonData: data,
-        contentType: 'application/json',
-    });
-}
-
-function storeLifetimeCampaignData(entityId, timestamp, data) {
-    if (!(data && data.length))
-        return Promise.resolve();
-
-    return bg.ajax(`${bg.serviceUrl}/api/data/${entityId}?timestamp=${timestamp}`, {
-        method: 'PUT',
-        jsonData: data,
-        contentType: 'application/json',
-    });
-}
-
-function storeCampaignStatus(entityId, timestamp, data) {
-    return bg.ajax(`${bg.serviceUrl}/api/campaignStatus/${entityId}?timestamp=${timestamp}`, {
-        method: 'PUT',
-        jsonData: data,
-        contentType: 'application/json',
-    });
-}
-
-async function storeKeywordData(entityId, adGroupId, timestamp, data) {
-    // Chop the large keyword list into small, bite-sized chunks for easier
-    // digestion on the server.
-    for (const chunk of common.pageArray(data, 50)) {
-        await bg.ajax(`${bg.serviceUrl}/api/keywordData/${entityId}/${adGroupId}?timestamp=${timestamp}`, {
-            method: 'PUT',
-            jsonData: chunk,
-            contentType: 'application/json',
-        });
-    }
-}
-
-function getMissingDates(entityId) {
-    return bg.ajax(`https://${constants.hostname}/api/campaignData/${entityId}/missingDates`, {
-        method: 'GET',
-        responseType: 'json',
-    });
-}
-
-const getCampaignHistory = bg.cache.coMemo(function(entityId, campaignId) { // TODO: date ranges, etc.
-    checkEntityId(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/data/${entityId}/${campaignId}`, { 
-        method: 'GET',
-        responseType: 'json'
-    });
-});
-
-const getAllCampaignData = bg.cache.coMemo(function({ entityId, startTimestamp, endTimestamp }) {
-    checkEntityId(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/data/${entityId}?startTimestamp=${startTimestamp}&endTimestamp=${endTimestamp}`, { 
-        method: 'GET',
-        responseType: 'json'
-    });
-});
-
-const getDataHistory = bg.cache.coMemo(async function({ entityId, campaignId }) { // TODO: date ranges, etc.
-    const snapshots = await getCampaignHistory(entityId, campaignId);
-    return common.convertSnapshotsToDeltas(snapshots);
-});
-
-const getAggregateCampaignHistory = bg.cache.coMemo(async function({ entityId, campaignIds }) {
-    let aggregate = [];
-    await bg.parallelQueue(campaignIds, async function(campaignId) {
-        try {
-            let history = await getDataHistory({ entityId, campaignId });
-            aggregate = aggregate.concat(...history);
-        }
-        catch (ex) {
-            if (bg.handleServerErrors(ex) == 'notAllowed') {
-                // swallow this
-            }
-            throw ex;
-        }
-    });
-
-    return aggregate.sort((a, b) => a.timestamp - b.timestamp);
-});
-
-const getKeywordData = bg.cache.coMemo(async function({ domain, entityId, campaignId, adGroupId }) {
-    checkEntityId(entityId);
-    const url = `${bg.serviceUrl}/api/keywordData/${entityId}/${adGroupId}`;
-    const opts = {
-        method: 'GET',
-        responseType: 'json',
-    };
-    let data = await bg.ajax(url, opts);
-
-    if (!data || data.length == 0) {
-        // Possibly this is the first time we've ever seen this campaign. If so,
-        // let's query Amazon and populate our own servers, and then come back.
-        // This is very slow but should usually only happen once.
-
-        const collector = await getCollector(domain, entityId);
-        try {
-            await requestKeywordDataImpl(collector, campaignId, adGroupId);
-            data = await bg.ajax(url, opts);
-        }
-        catch (ex) {
-            ga.merror(ex, `context: domain ${domain}, entityId ${entityId}, campaignId ${campaignId}, adGroupId ${adGroupId}, collector ${collector.name}`);
-        }
-    }
-
-    return data;
-});
-
-const getAggregateKeywordData = bg.cache.coMemo(function({ domain, entityId, adGroupIds }) {
-    return bg.parallelQueue(adGroupIds, async function(adGroupId) {
-        try {
-            return await getKeywordData({ domain, entityId, adGroupId });
-        }
-        catch (ex) {
-            if (bg.handleServerErrors(ex) == 'notAllowed') {
-                // swallow this
-            }
-            throw ex;
-        }
-    });
-});
-
-function storeCampaignMetadata(entityId, campaignId, asin) {
-    checkEntityId(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/campaignMetadata/${entityId}/${campaignId}`, {
-        method: 'PUT',
-        jsonData: { asin },
-    });
-}
-
-function storeAdGroupMetadata({ entityId, adGroupId, campaignId }) {
-    checkEntityId(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/adGroupMetadata/${entityId}/${adGroupId}`, {
-        method: 'PUT',
-        jsonData: { campaignId },
-    });
-}
-
-function storeEntityMetadata(metadata) {
-    const entityId = metadata.entityId;
-    checkEntityId(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/meta/${entityId}`, {
-        method: 'PUT',
-        jsonData: metadata,
-    });
-}
-
-function getAdGroups(entityId) {
-    checkEntityId(entityId);
-    return bg.ajax(`${bg.serviceUrl}/api/adGroups/${entityId}`, {
-        method: 'GET',
-        responseType: 'json',
-    });
 }
 
 async function updateKeyword({ domain, entityId, entity, keywords, operation, dataValues }) {
@@ -486,24 +119,15 @@ async function updateKeyword({ domain, entityId, entity, keywords, operation, da
         });
     }
 
-    if (result.ok.length)
-        getKeywordData.clear();
     return result;
 }
 
-async function addKeywords({ entity, entityId, domain, campaignId, adGroupId, keywords }) {
-    // simultaneous compat with plugin and page. TODO: delete this later
-    if (!entity) {
-        entity = { domain, entityId };
-    }
-
+async function addKeywords({ entity, adGroupId, keywords }) {
     const collector = await getCollector(entity.domain, entity.entityId);
-    campaignId = spData.stripPrefix(campaignId);
     adGroupId = spData.stripPrefix(adGroupId);
     keywords = _.uniqBy(keywords, kw => kw.keyword);
 
     const result = await collector.addKeywords({ keywords, adGroupId });
-    await requestKeywordDataImpl(collector, campaignId, adGroupId);
 
     return result;
 }
@@ -531,14 +155,6 @@ async function requestPortfolios({ entity }) {
 module.exports = {
     name: 'sp',
     addKeywords,
-    dataGather,
-    getAggregateCampaignHistory,
-    getAggregateKeywordData,
-    getAllCampaignData,
-    getAllowedCampaigns, 
-    getCampaignSummaries, 
-    getDataHistory,
-    getKeywordData,
     requestAdEntities,
     requestAdGroupId,
     requestCampaignAsin,
@@ -547,7 +163,6 @@ module.exports = {
     requestLifetimeCampaignData,
     requestPortfolios,
     setBrandName,
-    storeAdGroupMetadata,
     updateCampaigns,
     updateKeyword,
 };
