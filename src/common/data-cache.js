da6fa6cbdab69cc7f9fd654d04461284f2136f1a
JS Bangs <jaspax@gmail.com>
2019-06-24 21:35:31 +0300

    removed unused things from common functions

--- src/common/data-cache.js
+++ src/common/data-cache.js
@@ -1,29 +1,10 @@
-const memoize = require('memoizee');
-const constants = require('./constants.js');
-
 module.exports = function() {
-
-    const memoized = [];
-    const memoOptsDefault = {
-        promise: true,
-        length: false,
-        primitive: true,
-        normalizer: JSON.stringify,
-        maxAge: 6 * constants.timespan.hour,
-    };
-
-    function coMemo(fn, opts = {}) {
-        const rv = memoize(fn, Object.assign({}, memoOptsDefault, opts));
-        memoized.push(rv);
-        return rv;
-    }
-
-    function clear() {
-        memoized.forEach(fn => fn.clear());
+    function coMemo(fn) {
+        return fn;
     }
 
     return {
         coMemo,
-        clear,
+        clear: () => {}, // eslint-disable-line no-empty-function
     };
 };
